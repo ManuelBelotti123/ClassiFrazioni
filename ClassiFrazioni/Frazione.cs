daded5f3edef310299c2838559334d69e69cf2e0
Belotti.20181 <Belotti.20181@E2-9-PC19>
2023-10-31 13:17:50 +0100

    Attributi, Costruttori (anche di copia), Proprietà

--- ClassiFrazioni/Frazione.cs
+++ ClassiFrazioni/Frazione.cs
@@ -8,6 +8,47 @@ namespace ClassiFrazioni
 {
     internal class Frazione
     {
+        //attributi
+        private int _numeratore;
+        private int _denominatore;
 
+        //costruttori
+        public Frazione()
+        {
+            Numeratore = 0;
+            Denominatore = 0;
+        }
+        public Frazione(int num, int den)
+        {
+            Numeratore = num;
+            Denominatore = den;
+        }
+        public Frazione(Frazione frprec)
+        {
+            Numeratore = frprec.Numeratore;
+            Denominatore = frprec.Denominatore;
+        }
+
+        //proprietà
+        public int Numeratore
+        {
+            get { return _numeratore; }
+            set { _numeratore = value; }
+        }
+        public int Denominatore
+        {
+            get { return _denominatore; }
+            set 
+            {
+                if (value != 0)
+                {
+                    _denominatore = value;
+                }
+                else
+                {
+                    _denominatore = 0;
+                }
+            }
+        }
     }
 }
